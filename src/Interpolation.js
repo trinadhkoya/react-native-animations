e897781639d8777267b126e62e28265edd96a9c2
Trinadh Koya <trinadhkoya9@gmail.com>
2019-06-21 11:38:05 +0530

    Interpolation with height and width

--- src/Interpolation.js
+++ src/Interpolation.js
@@ -17,24 +17,36 @@ export default class Interpolation extends Component {
         duration: 500
       }),
       Animated.timing(this.state.animation, {
-        toValue: 2,
+        toValue: 0,
         duration: 1000
       })
     ]).start();
-
-
-
   };
 
   render() {
     const boxInterpolation = this.state.animation.interpolate({
       inputRange: [0, 1],
-      outputRange: ["0deg", "180deg"]
+      // outputRange: ["0deg", "180deg"],//will spin
+      outputRange: ["20%", "100%"] //will spin
     });
     const colorInterpolation = this.state.animation.interpolate({
       inputRange: [0, 1],
       outputRange: ["#000", "#fff"]
     });
+    const heightInterpolation = this.state.animation.interpolate({
+      inputRange: [0, 1],
+      // outputRange: ["0deg", "180deg"],//will spin
+      outputRange: ["10%", "100%"] //will spin
+    });
+    const widthInerpolation = this.state.animation.interpolate({
+      inputRange: [0, 1],
+      outputRange: ["10%", "100%"]
+    });
+
+    const boxStyles = {
+      width: widthInerpolation,
+      height: heightInterpolation
+    };
 
     const animatedStyles = {
       transform: [{ rotate: boxInterpolation }]
@@ -46,11 +58,11 @@ export default class Interpolation extends Component {
     return (
       <View style={styles.container}>
         <TouchableWithoutFeedback onPress={this.startAnimation}>
-          <Animated.View style={[styles.box, animatedStyles]}>
+          <Animated.View style={[styles.box, animatedStyles, boxStyles]}>
             <Animated.Text
               style={[textAnimatedStyles, { justifyContent: "center" }]}
             >
-              Trinadh Koya
+              TinMen
             </Animated.Text>
           </Animated.View>
         </TouchableWithoutFeedback>
